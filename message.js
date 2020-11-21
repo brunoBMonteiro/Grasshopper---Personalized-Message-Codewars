5186ad411d92cc6ab69587b69e65c1e1f0f291c1
Bruno Bilheri <52968377+brunoBMonteiro@users.noreply.github.com>
2020-11-21 10:19:20 -0300

    Solution finalized

---
+++ message.js
@@ -0,0 +1,23 @@
+//Personalized greeting
+//Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+//Use conditionals to return the proper message:
+
+//case	return
+//name equals owner	'Hello boss'
+//otherwise	'Hello guest'
+
+//function greet (name, owner) {
+  // Add code here
+//}
+
+
+//solution 
+function greet (name, owner) {
+    if (name === owner) {
+      return 'Hello boss'
+    }
+    else {
+      return 'Hello guest'
+    }
+  }
